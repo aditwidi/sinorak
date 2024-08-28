835bec4cc42e04c488658c289287f69213e7c486
Aditya <adit.widi@gmail.com>
2024-08-28 23:12:12 +0700

    restrict the /reset-password

--- src/components/ResetPassword.tsx
+++ src/components/ResetPassword.tsx
@@ -1,63 +1,77 @@
 // components/ResetPassword.tsx
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import Image from 'next/image';
-import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
-import Swal from 'sweetalert2'; // Import SweetAlert2
+import { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import Image from "next/image";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
+import Swal from "sweetalert2";
 import { LoadingIndicator } from "./LoadingIndicator";
 
 export default function ResetPassword() {
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
   const [showNewPassword, setShowNewPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const searchParams = useSearchParams();
-  const userId = searchParams.get('userId');
+  const userId = searchParams.get("userId");
   const router = useRouter();
+
+  // Redirect if no userId is present
+  useEffect(() => {
+    if (!userId) {
+      router.push("/error"); // Replace with your desired error or redirect page
+    }
+  }, [userId, router]);
+
+  // If userId is not found, prevent rendering of the component
+  if (!userId) {
+    return null; // Optionally render a loading spinner or a message here
+  }
 
   const handleResetPassword = async (e: React.FormEvent) => {
     e.preventDefault();
-    setError('');
+    setError("");
 
     // Check if passwords match
     if (newPassword !== confirmPassword) {
-      setError('Passwords do not match.');
+      setError("Passwords do not match.");
       return;
     }
 
     setLoading(true);
 
     try {
-      const response = await fetch('/api/forgot-password/update-password', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+      const response = await fetch("/api/forgot-password/update-password", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ userId, newPassword }),
       });
 
       setLoading(false);
 
       if (!response.ok) {
-        throw new Error('Failed to update password.');
+        throw new Error("Failed to update password.");
       }
 
       // Display SweetAlert on success
       Swal.fire({
-        title: 'Berhasil!',
-        text: 'Password berhasil terupdate.',
-        icon: 'success',
-        confirmButtonText: 'OK',
+        title: "Berhasil!",
+        text: "Password berhasil terupdate.",
+        icon: "success",
+        confirmButtonText: "OK",
         customClass: {
-          confirmButton: 'bg-indigo-600 hover:bg-indigo-700 text-white rounded-md px-4 py-2 mt-2',
+          confirmButton:
+            "bg-indigo-600 hover:bg-indigo-700 text-white rounded-md px-4 py-2 mt-2",
         },
       }).then(() => {
-        router.push('/sign-in'); // Redirect to sign-in page after closing alert
+        router.push("/sign-in"); // Redirect to sign-in page after closing alert
       });
     } catch (err) {
-      const errorMessage = err instanceof Error ? err.message : 'An unknown error occurred';
+      const errorMessage =
+        err instanceof Error ? err.message : "An unknown error occurred";
       setError(errorMessage);
     }
   };
@@ -65,7 +79,13 @@ export default function ResetPassword() {
   return (
     <div className="w-full max-w-xs p-4 space-y-6 bg-white rounded-lg shadow-md sm:max-w-sm md:max-w-md sm:p-6 lg:p-8">
       <div className="flex flex-col items-center">
-        <Image src="/images/logo2.png" alt="Logo" width={200} height={100} className="mb-4" />
+        <Image
+          src="/images/logo2.png"
+          alt="Logo"
+          width={200}
+          height={100}
+          className="mb-4"
+        />
         <h2 className="text-xl font-bold text-gray-900 sm:text-2xl mb-2 self-start">
           Reset Password
         </h2>
@@ -76,14 +96,17 @@ export default function ResetPassword() {
 
       <form className="mt-4 space-y-4" onSubmit={handleResetPassword}>
         <div>
-          <label htmlFor="new-password" className="block text-sm font-medium text-gray-700">
+          <label
+            htmlFor="new-password"
+            className="block text-sm font-medium text-gray-700"
+          >
             Password Baru
           </label>
           <div className="relative mt-1">
             <input
               id="new-password"
               name="new-password"
-              type={showNewPassword ? 'text' : 'password'}
+              type={showNewPassword ? "text" : "password"}
               required
               value={newPassword}
               onChange={(e) => setNewPassword(e.target.value)}
@@ -97,20 +120,27 @@ export default function ResetPassword() {
               className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 hover:text-indigo-500 focus:outline-none"
               aria-label="Toggle New Password Visibility"
             >
-              {showNewPassword ? <EyeSlashIcon className="h-5 w-5" /> : <EyeIcon className="h-5 w-5" />}
+              {showNewPassword ? (
+                <EyeSlashIcon className="h-5 w-5" />
+              ) : (
+                <EyeIcon className="h-5 w-5" />
+              )}
             </button>
           </div>
         </div>
 
         <div>
-          <label htmlFor="confirm-password" className="block text-sm font-medium text-gray-700">
+          <label
+            htmlFor="confirm-password"
+            className="block text-sm font-medium text-gray-700"
+          >
             Konfirmasi Password Baru
           </label>
           <div className="relative mt-1">
             <input
               id="confirm-password"
               name="confirm-password"
-              type={showConfirmPassword ? 'text' : 'password'}
+              type={showConfirmPassword ? "text" : "password"}
               required
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
@@ -124,7 +154,11 @@ export default function ResetPassword() {
               className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 hover:text-indigo-500 focus:outline-none"
               aria-label="Toggle Confirm Password Visibility"
             >
-              {showConfirmPassword ? <EyeSlashIcon className="h-5 w-5" /> : <EyeIcon className="h-5 w-5" />}
+              {showConfirmPassword ? (
+                <EyeSlashIcon className="h-5 w-5" />
+              ) : (
+                <EyeIcon className="h-5 w-5" />
+              )}
             </button>
           </div>
         </div>
@@ -137,14 +171,17 @@ export default function ResetPassword() {
             className="w-full flex justify-center rounded-md bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:bg-indigo-400"
             disabled={loading}
           >
-            {loading ? <LoadingIndicator /> : 'Update Password'}
+            {loading ? <LoadingIndicator /> : "Update Password"}
           </button>
         </div>
       </form>
 
       <p className="mt-6 text-center text-sm text-gray-600">
-        Kembali ke halaman{' '}
-        <a href="/sign-in" className="font-semibold text-indigo-600 hover:text-indigo-500">
+        Kembali ke halaman{" "}
+        <a
+          href="/sign-in"
+          className="font-semibold text-indigo-600 hover:text-indigo-500"
+        >
           Masuk
         </a>
       </p>
